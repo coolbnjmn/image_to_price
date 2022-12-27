81bb574a57dcfcac2efd77c783872a7178274e9f
coolbnjmn <hendricksbenjamin@gmail.com>
2022-12-27 09:17:23 -0600

    get script working with vision, setup for scrape step

--- README.md
+++ README.md
@@ -14,8 +14,14 @@ Python v3.9.7
 
 brew install poppler
 
-pip install --upgrade tensorboard
-pip install --upgrade setuptools
-pip install --upgrade google-api-python-client
-pip install --upgrade google-cloud-vision
-pip install pdf2image
+python3 -m pip install --upgrade tensorboard
+python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade google-api-python-client
+python3 -m pip install --upgrade google-cloud-vision
+python3 -m pip install pdf2image
+
+#### Usage
+
+Example:
+
+python3 pdf_to_csv.py pdfs/sports_pdf_test_1.pdf 
--- pdf_to_csv.py
+++ pdf_to_csv.py
@@ -11,6 +11,9 @@ from pdf2image import convert_from_path
 from google.oauth2 import service_account
 from google.cloud import vision
 
+## annotate & report functions from this Tutorial:
+## https://cloud.google.com/vision/docs/internet-detection?_gl=1*q4g24p*_ga*MTQ5NjY0OTc5NC4xNjcxOTkyNTYx*_ga_WH2QY8WWF5*MTY3MjA2OTI5NS4yLjEuMTY3MjA2OTM4MS4wLjAuMA..&_ga=2.101522433.-1496649794.1671992561
+
 def annotate(path):
     """Returns web annotations given the path to an image."""
     client = vision.ImageAnnotatorClient()
@@ -62,29 +65,36 @@ def report(annotations):
             print('Description: {}'.format(entity.description))
 
 
+def extract_and_annotate(pdf_file):
+    pdf_name, _ = os.path.splitext(pdf_file)
+    pages = convert_from_path(pdf_file)
+    with open(f'{pdf_name}_results.csv', 'w', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=['page', 'url', 'score', 'source'])
+        writer.writeheader()
+
+        for i, page in enumerate(pages):
+            png_file = f'{pdf_name}_page_{i+1}.png'
+            page.save(png_file, 'PNG')
+            result = annotate(png_file)
+            if result is None:
+                continue
+            report(result)
+
+            for image in result.full_matching_images:
+                writer.writerow({'page':i, 'url':image.url, 'score':1, 'source': pdf_name})
+            for image in result.pages_with_matching_images:
+                writer.writerow({'page':i, 'url':image.url, 'score':0.5, 'source': pdf_name})
+    print(f'Results saved to {pdf_name}_results.csv')
+    scrape_for_price(f'{pdf_name}_results.csv')
+
+def scrape_for_price(csv):
+    # TODO
+    return 0
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: python pdf_web_detection.py <PDF file>")
         sys.exit()
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='credentials/fantasyfootball-337700-deea52633b6d.json'
-    pdf_file = sys.argv[1]
-    pdf_name, _ = os.path.splitext(pdf_file)
-    pages = convert_from_path(pdf_file)
-    results = []
-    for i, page in enumerate(pages):
-        png_file = f'pngs/{pdf_name}_page_{i+1}.png'
-        page.save(png_file, 'PNG')
-        result = annotate(png_file)
-        if result is None:
-            continue
-        report(result)
-        results.append(result)
-    with open(f'{pdf_name}_results.csv', 'w', newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=['page', 'url', 'score'])
-        writer.writeheader()
-        for result in results:
-            for image in result.full_matching_images:
-                writer.writerow({'page':i, 'url':image.url, 'score':1})
-            for image in result.pages_with_matching_images:
-                writer.writerow({'page':i, 'url':image.url, 'score':0.5})
-    print(f'Results saved to {pdf_name}_results.csv')
+    extract_and_annotate(sys.argv[1])
+    
